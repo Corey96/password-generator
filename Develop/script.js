305cf7fe14fd1a9f20c49149e9de8d2d6d5d0f04
Corey96 <0coreyy@gmail.com>
2022-01-03 16:35:31 +0000

    added if statements with confirms and a string for charset

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-const characters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
-const charactersUpper = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"
-const numbers = "0,1,2,3,4,5,6,7,8,9"
-const specialCharacters = "~`!@#$%^&*()_-+={[}]|\:;'<,>.?/";
-const passwordLength = 128
-let passwordValue = ''
+var passwordCharSet = '';
+const key_strings = {
+  lowercase: "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z",
+  uppercase: "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z",
+  numbers: "0,1,2,3,4,5,6,7,8,9",
+  symbols: "~`!@#$%^&*()_-+={[}]|\:;'<,>.?/",
+}
 
 function generatePassword() {
-console.log("Button clicked")
+// Check to see button works
+console.log("Button Clicked :)")
 
-let userChoice = window.prompt ("Enter password length between 0 and 128 characters");
-let specialCharacters = window.prompt ("Include special characters?");
-  if ("yes") then 
-let incNumbers = window.prompt ("Include numbers?") ; 
-  
-  
+var length = window.prompt ("Length of password between 8 - 128 characters", "Enter Here")
 
+var lowercase = window.confirm("Would you like to use lowercase letters?");
+if (lowercase) {
+  passwordCharSet += key_strings.lowercase;
+};
 
-return ("generated password will go here")
+var uppercase = window.confirm("Would you like to use upper case letters?");
+if (uppercase) {
+  passwordCharSet += key_strings.uppercase;
+};
+
+var symbols = window.confirm ("Would you like to use symbols?");
+if (symbols) {
+  passwordCharSet += key_strings.symbols
 }
+
+var numbers = window.confirm ("Would you like to use numbers?");
+if (numbers) {
+  passwordCharSet += key_strings.numbers;
+};
+
+var password = "";
+for (let i = 0; i < length; i++) {
+  password += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
+}
+
+return password;
+
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -32,3 +55,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
